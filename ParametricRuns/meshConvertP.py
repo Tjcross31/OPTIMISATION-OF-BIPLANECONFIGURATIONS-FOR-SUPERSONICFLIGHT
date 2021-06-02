16d73133645eb84487aeaf59bca0997df28ac63f
Tjcross31 <62484619+Tjcross31@users.noreply.github.com>
2021-06-02 02:22:35 +0100

    Add files via upload

---
+++ ParametricRuns/meshConvertP.py
@@ -0,0 +1,20 @@
+import os
+from multiprocessing import Process
+ 
+ 
+def f(name):
+    if os.path.isdir(name) and name != "blank_foamcase":
+        oldDir = os.getcwd()
+        os.system('cd ~/OpenFOAM/tjc2017-7/run')
+        os.chdir(oldDir)
+        os.chdir(name)
+        os.system('gmshToFoam busemann.msh')
+        os.chdir(oldDir)
+ 
+ 
+if __name__ == "__main__":
+    dirs = os.listdir()
+    for name in dirs:
+        p = Process(target=f, args=(name,))
+        p.start()
+
